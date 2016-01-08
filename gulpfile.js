1473590e42c308b57779710f6c50d3aa125c927b
Jeremy Gray <speeqr@gmail.com>
2016-01-07 16:17:22 -0800

    integrates bourbon, neat and connect into working framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp'),
   gutil = require('gulp-util'),
-  jshint = require('gulp-jshint'),
+  //jshint = require('gulp-jshint'),
   browserify = require('gulp-browserify'),
-  compass = require('gulp-ruby-sass'),
+  sass = require('gulp-sass'),
   connect = require('gulp-connect'),
   gulpif = require('gulp-if'),
   uglify = require('gulp-uglify'),
   minifyHTML = require('gulp-htmlmin'),
   jsonminify = require('gulp-jsonminify'),
   concat = require('gulp-concat');
+
+var bourbon = require('node-bourbon');
+  bourbon.includePaths
+var neat = require('node-neat');
+  neat.includePaths // Array of Neat paths (including Bourbon)
+
+  //gutil.log ("THIS IS MY BOURBON + NEAT: " + neat.includePaths);
 
 var env,
   jsSources,
@@ -50,12 +57,15 @@ gulp.task('jshint', function() {
     // .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
 });
 
-gulp.task('compass', function() {
-   return compass(sassSources, {
-     compass: true,
-     style: sassStyle
-   }).on('error', gutil.log)
-   .pipe(gulp.dest(outputDir + 'css'))
+gulp.task('sass', function () {
+  gulp.src(sassSources)
+    .pipe(sass({
+      includePaths: neat.includePaths
+    }))
+    .pipe(sass({outputStyle: sassStyle}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(connect.reload());
 });
 
 gulp.task('html', function() {
@@ -74,7 +84,7 @@ gulp.task('json', function() {
 
 gulp.task('watch', function() {
   gulp.watch(jsSources, ['js']);
-  gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch('components/sass/*.scss', ['sass']);
   gulp.watch('builds/development/*.html', ['html']);
   gulp.watch(jsonSources, ['json']);
 });
@@ -86,4 +96,4 @@ gulp.task('connect', function() {
   })
 });
 
-gulp.task('default', ['html', 'json', 'js', 'jshint', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'js', 'sass', 'connect', 'watch']);
